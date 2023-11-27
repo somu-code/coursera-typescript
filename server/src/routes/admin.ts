d843b4dde61d3f5e9b8ade11b10642243fb28114
Ankit Kumar <ankitlucky.kumar99@gmail.com>
2023-11-27 11:14:41 +0100

    Added Basic Routes

--- server/src/routes/admin.ts
+++ server/src/routes/admin.ts
@@ -1,11 +1,18 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
 export const adminRouter = Router();
 
-adminRouter.get("/", async (req, res) => {
+adminRouter.get("/profile", async (req: Request, res: Response) => {
   try {
-    res.status(200).send("<h1>Admin api</h1>");
+    res.status(200).send("Admin api");
   } catch (error) {
     res.sendStatus(500);
   }
 });
+adminRouter.post("/login", async(req: Request, res: Response)=>{
+
+})
+
+adminRouter.post("/signUp", async(req: Request, res: Response)=>{
+
+})
