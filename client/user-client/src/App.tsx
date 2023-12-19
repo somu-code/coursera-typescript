b5732eb68030ae728524de8e411c2a22b27e5073
Somnath Golui <code.somu@gmail.com>
2023-12-19 16:37:20 +0530

    React route dom is wokring. Done with the Signup page.

--- client/user-client/src/App.tsx
+++ client/user-client/src/App.tsx
@@ -1,7 +1,9 @@
+import Home from "./components/Home";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-center">User</h1>
+      <Home />
     </>
   );
 }
---
+++ client/user-client/src/components/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="">
+      <div className="">
+        <h1 className="text-[#1E0E62] text-center text-7xl font-bold mt-36">
+          Course Selling Platform
+        </h1>
+        <p className="text-[#151439] text-center text-xl font-medium max-w-xl mx-auto mt-12">
+          We made it so beautiful and simple. It combines courses, adds courses,
+          updates and deletes. So you can focus on the teaching part. It is
+          definitely the tool you need in your collection!
+        </p>
+      </div>
+    </div>
+  );
+}
