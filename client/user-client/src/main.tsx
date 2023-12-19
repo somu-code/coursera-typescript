47ee0fae5a2f283cc85fffa51904799ead840f68
Somnath Golui <code.somu@gmail.com>
2023-12-19 16:50:25 +0530

    Signup route is working properly adn also coded the Signin route.

--- client/user-client/src/main.tsx
+++ client/user-client/src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar.tsx";
 import Signup from "./pages/Signup.tsx";
+import Signin from "./pages/Signin.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
