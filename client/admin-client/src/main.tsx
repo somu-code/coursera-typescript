40fe73c9eafa87b11c71c059e0f3ad85139f90ec
Somnath Golui <code.somu@gmail.com>
2024-01-11 16:46:51 +0530

    formated the code

--- client/admin-client/src/main.tsx
+++ client/admin-client/src/main.tsx
@@ -1,12 +1,12 @@
 /** @format */
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Navbar } from './components/Navbar.tsx';
-import SignInPage from './components/SignInPage.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar.tsx";
+import SignInPage from "./components/SignInPage.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
