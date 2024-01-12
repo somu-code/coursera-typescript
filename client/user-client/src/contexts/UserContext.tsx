ca6dbd98bff796bbcdcff3dc76aefdccaf512d38
Ankit Kumar <ankitlucky.kumar99@gmail.com>
2024-01-12 18:46:42 +0100

    Adds ContextAPI...

---
+++ client/user-client/src/contexts/UserContext.tsx
@@ -0,0 +1,43 @@
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useState,
+} from "react";
+
+export type User = {
+  email: string;
+  name: string;
+};
+
+export interface UserContextInterface {
+  user: User;
+  setUser: Dispatch<SetStateAction<User>>;
+}
+
+const defaultState = {
+  user: {
+    name: "",
+    email: "",
+  },
+  setUser: (user: User) => {},
+} as UserContextInterface;
+
+export const UserContext = createContext(defaultState);
+
+type UserProvideProps = {
+  children: ReactNode;
+};
+
+export default function UserProvider({ children }: UserProvideProps) {
+  const [user, setUser] = useState<User>({
+    name: "",
+    email: "",
+  });
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
